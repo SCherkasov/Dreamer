ba1b66290d55bf03219904dcc54256a58459bcc9
Stasik <scherkasov8@gmail.com>
2017-08-23 16:47:30 +0300

    Added tableView, his protocols, Video_3

---
+++ Dreamer/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  Dreamer
+//
+//  Created by Stanislav Cherkasov on 23.08.17.
+//  Copyright © 2017 Stanislav Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+  
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
